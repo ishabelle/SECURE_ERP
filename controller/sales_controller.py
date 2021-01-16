89b6b4f514d9b5acb87089da479dee76078f08f4
ishabelle <kolodzinska.izabela@gmail.com>
2021-01-16 14:16:11 +0100

    fix def add_transaction()

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -1,5 +1,3 @@
-from model import data_manager
-from model import util
 from model.sales import sales 
 from view import terminal as view
 
@@ -10,107 +8,111 @@ INDEX_COL_PRODUCT = 2
 INDEX_COL_PRICE = 3
 INDEX_COL_DATE = 4
 
-# READ from CRUD
-def list_transactions(): #DZIAŁĄ
+def list_transactions(): #DZIAŁA
     """ Opening transactions list """
 
-    print("\nLIST TRANSACTION\n")
-    transactions = data_manager.read_table_from_file(sales.DATAFILE, separator=";")
-    view.print_table(transactions, sales.HEADERS)
+    view.print_message("\nLIST TRANSACTION\n")
+    transactions_list = sales.read()
+    view.print_table(transactions_list, sales.HEADERS)
     
 
-# CREATE from CRUD
-def add_transaction(): #NIE DZIAŁA
+def add_transaction(): #DZIAŁA
     """ Adding new element to list transactions """
-    
-    transactions = data_manager.read_table_from_file(sales.DATAFILE, separator=";")
-    transaction_id = util.generate_id()
-    customer_id = util.generate_id()
+
+    view.print_message("\nADDING NEW TRANSACTIONS\n")
     product = view.get_input("Please enter product name: ")
     price = view.get_input("Please enter price of product: ")
     date = view.get_input("Please provide date of transaction (in format 2021-01-11): ")
-    tab = []
-    new_transacion = tab.extend([transaction_id, customer_id, product, price, date])
-    
-
-    
-
-    list_with_new_trans = data_manager.write_table_to_file(sales.DATAFILE, transactions, separator=";")
-    view.print_table(list_with_new_trans, sales.HEADERS)
+    sales.create(product, price, date)
+    view.print_table(sales.read(), sales.HEADERS)
     
     
 # UPDATE  from CRUD
-def update_transaction(): #DZIAŁA
+def update_transaction(): #NIE DZIAŁA
     """ Updating transaction list"""
+
+    view.print_message("\nUPDATING TRANSACTION LIST\n")
+    view.print_table(sales.read(), sales.HEADERS)
+    header = view.get_input("Enter number of transaction to update: ")
+    value = view.get_inputs(sales.HEADERS[2:5])
+    sales.update(id, header, value)
+    view.get_input("Transaction updated. Press enter")
     
-    transactions = data_manager.read_table_from_file(sales.DATAFILE)
-    user_choice = view.get_input("Please provide the ID's customer, which you want to update: ")
-    for row in transactions:
-        if user_choice == row[0]:
-            customer_id = util.generate_id()
-            product = view.get_input("Please enter product name: ")
-            price = view.get_input("Please enter price of product: ")
-            date = view.get_input("Please provide date of transaction (in format 2021-01-11): ")
-            row[INDEX_COL_CUSTOMER] = customer_id
-            row[INDEX_COL_PRODUCT] = product
-            row[INDEX_COL_PRICE] = price
-            row[INDEX_COL_DATE] = date
-            data_manager.write_table_to_file(sales.DATAFILE, transactions)
-    view.print_table(transactions, sales.HEADERS)
+    
+
+    
+    # sales.update(id, header, value)
+    # transactions = data_manager.read_table_from_file(sales.DATAFILE)
+    # user_choice = view.get_input("Please provide the ID's customer, which you want to update: ")
+    # for row in transactions:
+    #     if user_choice == row[0]:
+    #         customer_id = util.generate_id()
+    #         product = view.get_input("Please enter product name: ")
+    #         price = view.get_input("Please enter price of product: ")
+    #         date = view.get_input("Please provide date of transaction (in format 2021-01-11): ")
+    #         row[INDEX_COL_CUSTOMER] = customer_id
+    #         row[INDEX_COL_PRODUCT] = product
+    #         row[INDEX_COL_PRICE] = price
+    #         row[INDEX_COL_DATE] = date
+    #         data_manager.write_table_to_file(sales.DATAFILE, transactions)
+    # view.print_table(transactions, sales.HEADERS)
 
 
-# DELETE from CRUD
+
 def delete_transaction(): #DZIAŁA
     """ Removing transaction from the list of transaction """
 
-    transactions = data_manager.read_table_from_file(sales.DATAFILE)
-    user_choice = view.get_input("Please provide the ID's customer, which you want to delete: ")
-    for row in transactions:
-        if user_choice == row[0]:
-            deleted_row = row
-            print("\nChoosing transaction has been removed from list.\n")
-            transactions.remove(deleted_row)
-            data_manager.write_table_to_file(sales.DATAFILE, transactions)
-    view.print_table(transactions, sales.HEADERS)
+    view.print_message("\nREMOVING TRANSACTION\n")
+    id = view.get_input("Please provide the ID, which you want to update: ")
+    sales.delete(id)
+    view.print_table(sales.read(), sales.HEADERS)
+  
             
 
 def get_biggest_revenue_transaction(): #DZIAŁA
     """ Determining which transaction is the largest and makes the biggest revenue """
 
     income = []
-    transactions = data_manager.read_table_from_file(sales.DATAFILE)
+    transactions = sales.read()
     for row in transactions:
         income.append(row[INDEX_COL_PRICE])
     number_of_biggest_income = income.index(max(income))
     transaction = transactions[number_of_biggest_income]
     biggest_trans = " ".join(transaction)
-    print(f"\nTransaction that makes the biggest revenue is: {biggest_trans}\n")
+    view.print_message(f"\nTransaction that makes the biggest revenue is: {biggest_trans}\n")
     return biggest_trans
 
 
 def get_biggest_revenue_product():
     """ Determining which product makes the biggest revenue """
 
-    biggest_trans = get_biggest_revenue_transaction()
+    # biggest_trans = get_biggest_revenue_transaction()
+    income = []
+    transactions = sales.read()
+    for row in transactions:
+        income.append(row[INDEX_COL_PRICE])
+    number_of_biggest_income = income.index(max(income))
+    transaction = transactions[number_of_biggest_income]
+    biggest_trans = " ".join(transaction)
     product = biggest_trans[INDEX_COL_PRODUCT]
-    print(f"The product that makes the biggest revenue is: {product}")
+    view.print_message(f"\nThe product that makes the biggest revenue is: {product}\n")
     return product
 
 
-def count_transactions_between():
+def count_transactions_between(): #DZIAŁA
     """ Counting number of transactions between two given dates """
     
     dates = []
     first_date = view.get_input("Please provide first date (in format 2021-01-11): ")
     second_date = view.get_input("Please provide second date (in format 2021-01-11): ")
-    transactions = data_manager.read_table_from_file(sales.DATAFILE)
+    transactions = sales.read()
     for row in transactions:
         dates.append(row[-1])
     count = 0
     for date in dates:
         if date > first_date and date < second_date:
             count += 1
-    print(f"The number of transaction beetween {first_date} and {second_date} is {count}.")
+    view.print_message(f"\nThe number of transaction beetween {first_date} and {second_date} is {count}.\n")
 
 
 def sum_transactions_between():
@@ -120,7 +122,7 @@ def sum_transactions_between():
     trans_price = []
     trans_sum = []
     indexes = []
-    transactions = data_manager.read_table_from_file(sales.DATAFILE)
+    transactions = sales.read()
     first_date = view.get_input("Please provide first date (in format 2021-01-11): ")
     second_date = view.get_input("Please provide second date (in format 2021-01-11): ")
     for row in transactions:
@@ -133,7 +135,7 @@ def sum_transactions_between():
         trans_sum.append(trans_price[index])
     trans_sum = [float(trans) for trans in trans_sum]
     result = sum(trans_sum)
-    print(f"The sum beetween {first_date} and {second_date} is {result}.")
+    view.print_message(f"The sum beetween {first_date} and {second_date} is {result}.")
 
 
 def run_operation(option):
--- model/sales/sales.py
+++ model/sales/sales.py
@@ -12,3 +12,47 @@ from model import data_manager, util
 
 DATAFILE = "model/sales/sales.csv"
 HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
+
+# Create Read Update Delete
+
+def create(product, price, date):
+    table = read()
+    id = util.generate_id()
+    customer = util.generate_id()
+    data_to_add = [id, customer, product, price, date]
+    table.append(data_to_add)
+    data_manager.write_table_to_file(DATAFILE, table)
+
+
+def read(with_headers = False):
+    file_content = data_manager.read_table_from_file(DATAFILE)
+    if with_headers:
+        file_content = HEADERS + file_content
+    return file_content
+
+
+def update(id, header, value):
+    file_content = read()
+
+    id_index = HEADERS.index("Id")
+    header_index = HEADERS.index(header)
+
+    for element in file_content:
+        if element[id_index] == id:
+            element[header_index] = value
+
+    data_manager.write_table_to_file(DATAFILE, file_content)
+
+
+def delete(id):
+    table = read()
+    new_table = []
+    id_index = HEADERS.index("Id")
+
+    for element in table:
+        if element[id_index] != id:
+            new_table.append(element)
+
+    data_manager.write_table_to_file(DATAFILE, new_table)
+
+
